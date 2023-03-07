117c28bd03858b99940ae01b54ee262c1448b93e
Andrew Moss <andrew.moss@neofinancial.com>
2023-03-06 18:09:48 -0700

    chore: Adding editorconfig, fixing improper variable

--- flake.sh
+++ flake.sh
@@ -7,33 +7,33 @@ save_outputs=false
 
 # Parse options
 while getopts ":s:i:jh" opt; do
-    case $opt in
-    h)
-        echo "Usage: $0 [-s jest_script] [-i iterations] [-j] [-h]"
-        echo "  -s: Specify the jest test script (default: npm run test -- --json)"
-        echo "  -i: Specify the number of test iterations (default: 100)"
-        echo "  -j: Save jest --json outputs (default: false)"
-        echo "  -h: Display this help message"
-        exit 0
-        ;;
-    s)
-        jest_script="$OPTARG"
-        ;;
-    i)
-        iterations="$OPTARG"
-        ;;
-    j)
-        save_outputs=true
-        ;;
-    \?)
-        echo "Invalid option: -$OPTARG" >&2
-        exit 1
-        ;;
-    :)
-        echo "Option -$OPTARG requires an argument." >&2
-        exit 1
-        ;;
-    esac
+  case $opt in
+  h)
+    echo "Usage: $0 [-s jest_script] [-i iterations] [-j] [-h]"
+    echo "  -s: Specify the jest test script (default: npm run test -- --json)"
+    echo "  -i: Specify the number of test iterations (default: 100)"
+    echo "  -j: Save jest --json outputs (default: false)"
+    echo "  -h: Display this help message"
+    exit 0
+    ;;
+  s)
+    jest_script="$OPTARG"
+    ;;
+  i)
+    iterations="$OPTARG"
+    ;;
+  j)
+    save_outputs=true
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
+  :)
+    echo "Option -$OPTARG requires an argument." >&2
+    exit 1
+    ;;
+  esac
 done
 
 failures=()
@@ -42,35 +42,35 @@ echo "jest test script: $jest_script"
 echo "number of iterations: $iterations"
 
 for ((i = 1; i <= $iterations; i++)); do
-    echo "Running Jest test suite, iteration $i ..."
+  echo "Running Jest test suite, iteration $i ..."
 
-    jest_output=$($jest_script | tail -n +5)
+  jest_output=$($jest_script | tail -n +5)
 
-    if [ "$is_true" = true ]; then
-        output_file="test_results_$i.json"
-        echo "$jest_output" >$output_file
-    fi
+  if [ "$save_outputs" = true ]; then
+    output_file="test_results_$i.json"
+    echo "$jest_output" >$output_file
+  fi
 
-    if echo "$jest_output" | jq -e '.testResults[].assertionResults[] | select(.status=="failed")' >/dev/null; then
+  if echo "$jest_output" | jq -e '.testResults[].assertionResults[] | select(.status=="failed")' >/dev/null; then
 
-        failing_tests=$(echo "$jest_output" | jq -r '.testResults[].assertionResults[] | select(.status=="failed") | .fullName')
+    failing_tests=$(echo "$jest_output" | jq -r '.testResults[].assertionResults[] | select(.status=="failed") | .fullName')
 
-        readarray -t failing_tests_collection <<<"$failing_tests"
+    readarray -t failing_tests_collection <<<"$failing_tests"
 
-        for failure in "${failing_tests_collection[@]}"; do
-            failures+=("$failure")
-        done
+    for failure in "${failing_tests_collection[@]}"; do
+      failures+=("$failure")
+    done
 
-    fi
+  fi
 done
 
 declare -A freq
 
 for str in "${failures[@]}"; do
-    ((freq["$str"]++))
+  ((freq["$str"]++))
 done
 
 echo -e "String\tFrequency"
 for str in "${!freq[@]}"; do
-    echo -e "$str\t${freq[$str]}"
+  echo -e "$str\t${freq[$str]}"
 done
