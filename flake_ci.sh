7fbb77b75c73d59be5e4b9471c79ae6ead810334
Andrew Moss <andrew.moss@neofinancial.com>
2023-03-09 18:17:54 -0700

    chore: Simpler table for ci

--- flake_ci.sh
+++ flake_ci.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-print_frequency_table() {
-  declare -A freq
-  for str in "${@}"; do
-    ((freq["$str"]++))
-  done
-
-  max_len=0
-  for str in "${!freq[@]}"; do
-    len=${#str}
-    if ((len > max_len)); then
-      max_len=$len
-    fi
-  done
-
-  printf "| $(tput bold)$(tput setaf 6)%-${max_len}s $(tput sgr0)| $(tput bold)$(tput setaf 6)%-10s $(tput sgr0)|\n" "String" "Frequency"
-  echo "$(tput bold)$(tput setaf 6)+$(printf '%*s' "$((max_len + 2))" "" | tr ' ' '-')+$(printf '%*s' 12 "" | tr ' ' '-')+$(tput sgr0)"
-  for str in "${!freq[@]}"; do
-    printf "| $(tput setaf 2)%-${max_len}s $(tput sgr0)| $(tput setaf 5)%10d $(tput sgr0)|\n" "$str" "${freq[$str]}"
-  done
-  echo "$(tput bold)$(tput setaf 6)+$(printf '%*s' "$((max_len + 2))" "" | tr ' ' '-')+$(printf '%*s' 12 "" | tr ' ' '-')+$(tput sgr0)"
-}
-
 # Default values
 jest_script="npm run test -- --json"
 iterations=100
@@ -97,4 +75,14 @@ for ((i = 1; i <= $iterations; i++)); do
   fi
 done
 
-print_frequency_table "${failures[@]}"
+declare -A freq
+
+for str in "${failures[@]}"; do
+  ((freq["$str"]++))
+done
+
+# Print out the frequency table (simple)
+echo -e "String\tFrequency"
+for str in "${!freq[@]}"; do
+  echo -e "$str\t${freq[$str]}"
+done
